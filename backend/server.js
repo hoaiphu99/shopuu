2bd03d5de04d96280e1e2013ba1b543f74ab7067
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-08-31 15:08:50 +0700

    deploy

--- backend/server.js
+++ backend/server.js
@@ -26,16 +26,24 @@ app.use(express.json())
 // Routes init
 route(app)
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 app.use(notFound)
 
